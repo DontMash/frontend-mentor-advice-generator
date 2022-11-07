34f3ec118685eaae6ada2ae09ed54d48ac89a987
Sören Maschmann <smaschmann@slashwhy.de>
2022-11-07 16:55:00 +0100

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<main>
+  Advice #<!-- Advice ID goes here -->
+
+  "<!-- Advice text goes here -->"
+  
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,svelte}'
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'light-cyan': '#cee3e9',
+      'neon-green': '#52ffa8',
+      'grayish-blue': '#4e5d73',
+      'dark-grayish-blue': '#323a49',
+      'dark-blue': '#1f2632',
+    },
+    fontFamily: {
+      'manrope': ['Manrope', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
