e3c8d9dbc4911153341ab6876873148d165aad2e
Sören Maschmann <smaschmann@slashwhy.de>
2022-11-07 18:59:42 +0100

    Done

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,79 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import Footer from './lib/Footer.svelte';
+  import IconDice from './lib/icons/IconDice.svelte';
+
+  type Slip = {
+    id: number;
+    advice: string;
+  };
+  const API_URL: string = 'https://api.adviceslip.com/advice';
+
+  let slip: Slip;
+  const getSlip = async (): Promise<Slip> => {
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) {
+        throw new Error(`[${response.status}] ${response.statusText}`, {
+          cause: response.body,
+        });
+      }
+
+      const { slip } = await response.json();
+      return slip;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const rollDice = async () => {
+    slip = await getSlip();
+  };
+
+  onMount(async () => {
+    await rollDice();
+  });
 </script>
 
-<main>
-  Advice #<!-- Advice ID goes here -->
-
-  "<!-- Advice text goes here -->"
-  
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
-  </div>
+<main class="flex justify-center items-center min-h-screen p-4">
+  <h1 class="sr-only">Advice Generator</h1>
+  <article
+    class="relative flex flex-col justify-between max-w-sm min-h-[256px] px-6 md:px-10 pt-10 pb-12 mb-7 rounded-xl bg-dark-grayish-blue text-center shadow-xl space-y-4"
+  >
+    <a
+      class="transition-transform hover:scale-105"
+      href="https://api.adviceslip.com/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <h2 class="text-xs text-neon-green tracking-[0.3em] uppercase">
+        Advice #{slip?.id}
+      </h2>
+    </a>
+    <blockquote class="text-2xl before:content-['“'] after:content-['”']">
+      {slip?.advice}
+    </blockquote>
+    <picture class="block py-2">
+      <source srcset="/images/pattern-divider-desktop.svg" media="(min-width: 768px)" />
+      <source srcset="/images/pattern-divider-mobile.svg" />
+      <img
+        class="mx-auto"
+        src="/images/pattern-divider-mobile.svg"
+        alt=""
+        width="295"
+        height="16"
+      />
+    </picture>
+    <button
+      class="absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2"
+      on:click={(event) => rollDice()}
+    >
+      <div
+        class="p-4 rounded-full bg-neon-green transition-all hover:shadow-neon-green hover:shadow-glow hover:scale-105 motion-safe:animate-pop"
+      >
+        <span class="sr-only">Generate advice</span>
+        <IconDice />
+      </div>
+    </button>
+  </article>
 </main>
+<Footer />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,25 @@ module.exports = {
     './src/**/*.{ts,svelte}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'pop': 'pop 1s ease-in-out'
+      },
+      boxShadow: {
+        'glow': '0 0 32px -4px rgba(0, 0, 0, 0.3)'
+      },
+      keyframes: {
+        'pop': {
+          '0%, 100%': {
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            'transform': 'translateY(-10%) scale(1.05)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        }
+      }
+    },
     colors: {
       'light-cyan': '#cee3e9',
       'neon-green': '#52ffa8',
